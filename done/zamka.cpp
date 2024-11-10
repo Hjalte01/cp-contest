68c2e4a971705b6b3f4dadb7dd30291f4c95b787
hjalte01 <hjalte@bjoernstrup.net>
2024-11-10 02:52:32 +0100

    done today with Simon

--- done/zamka.cpp
+++ done/zamka.cpp
@@ -2,33 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int intCalc(int a) {
-    if (a == 0) return 0;
-    return a%10 + intCalc(a/10);
+  if (a == 0)
+    return 0;
+  return a % 10 + intCalc(a / 10);
 }
 
 int main() {
 
-    int l, d, x, n;
-    cin >> l >> d >> x;
+  int l, d, x, n;
+  cin >> l >> d >> x;
 
-    for (int i = l; i <= d; i++) {
-        if (intCalc(i) == x) {
-            cout << i << "\n";
-            break;
-        }
+  for (int i = l; i <= d; i++) {
+    if (intCalc(i) == x) {
+      cout << i << "\n";
+      break;
     }
+  }
 
-    for (int i = d; i >= l; i--) {
-        if (intCalc(i) == x) {
-            cout << i;
-            break;
-        }
+  for (int i = d; i >= l; i--) {
+    if (intCalc(i) == x) {
+      cout << i;
+      break;
     }
+  }
 
-
-
-
-    return 0;
+  return 0;
 }
